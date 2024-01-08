2a2d00106f120b0e8be457e624be90ba808cdd15
sophiemeakin <sophie.meakin@epicentre.msf.org>
2024-01-08 16:29:58 +0100

    Small fixes * use dplyr::bind_rows instead of data.table * make column scenario numeric (match scenario grid) * ref packages

--- R/evaluation.R
+++ R/evaluation.R
@@ -9,13 +9,15 @@
 #' @export
 summarise_results <- function(sweep_df) {
   
-  big_results <- rbindlist(sweep_df$sims, idcol = "scenario")
+  big_results <- dplyr::bind_rows(sweep_df$sims, .id = "scenario") %>%
+    dplyr::mutate(scenario = as.integer(scenario))
   
-  big_results <- big_results %>% 
-    group_by(scenario, iter) %>%
-    summarise(r0 = mean(new_cases, na.rm = TRUE),
-              n_pep = sum(n_hrc_pep, na.rm = TRUE),
-              outbreak_size = n())
+  out <- big_results %>% 
+    dplyr::group_by(scenario, iter) %>%
+    dplyr::summarise(
+      r0 = mean(new_cases, na.rm = TRUE),
+      n_pep = sum(n_hrc_pep, na.rm = TRUE),
+      outbreak_size = dplyr::n())
   
-  big_results
+  return(out)
 }
