7eed365d31b3e24d79a1e1d5dd2d0405a6fb7ff1
sophiemeakin <sophie.meakin@epicentre.msf.org>
2024-01-08 11:36:23 +0100

    Set up outbreak (generation 0)

--- R/outbreak_setup.R
+++ R/outbreak_setup.R
@@ -7,9 +7,25 @@
 #' @export
 #' 
 outbreak_setup <- function(
-    n_intialcases
+    n_initialcases
 ) {
   
   # function body; ref https://github.com/epiforecasts/ringbp/blob/main/R/outbreak_setup.R
+  
+  case_data <- data.frame(
+    generation = 0,
+    case_id = 1:n_initialcases,
+    case_type = NA,
+    infector = NA,
+    new_cases = NA,
+    new_cases_hrc = NA,
+    new_cases_pep = NA,
+    new_cases_lrc = NA,
+    n_hrc = NA,
+    n_hrc_pep = NA,
+    n_lrc = NA
+    )
+  
+  return(case_data)
   
 }
