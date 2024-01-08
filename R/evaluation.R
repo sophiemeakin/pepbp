57a04ef14e777fd3666741e657e8817735b2ef91
sophiemeakin <sophie.meakin@epicentre.msf.org>
2024-01-08 17:12:04 +0100

    Add documentation * + update variable name

--- R/evaluation.R
+++ R/evaluation.R
@@ -1,12 +1,19 @@
-#' Title
+#' Summarise results from parameter sweep
+#' @author Joel Hellewell, Sophie Meakin
+#' 
+#' @description Summarise results from parameter_sweep by scenario and iteration.
 #'
-#' @param sweep_df 
+#' @param sweep_df A data.frame, output from pepbp::parameter_sweep().
 #'
-#' @return
+#' @return A data.frame with columns:
+#' * scenario: scenario id
+#' * iter: iteration id
+#' * outbreak_size: total cases
+#' * n_pep: total number of HRCs that received PEP
+#' * r0: average number of secondary cases
+#' 
 #' @export
 #'
-#' @examples
-#' @export
 summarise_results <- function(sweep_df) {
   
   big_results <- dplyr::bind_rows(sweep_df$sims, .id = "scenario") %>%
@@ -15,9 +22,10 @@ summarise_results <- function(sweep_df) {
   out <- big_results %>% 
     dplyr::group_by(scenario, iter) %>%
     dplyr::summarise(
+      outbreak_size = dplyr::n(),
+      n_pep = sum(n_pep, na.rm = TRUE),
       r0 = mean(new_cases, na.rm = TRUE),
-      n_pep = sum(n_hrc_pep, na.rm = TRUE),
-      outbreak_size = dplyr::n())
+      )
   
   return(out)
 }
