dc4fb2f3fb6ad79bb3f581aeae9854b3d1f1ed3f
sophiemeakin <sophie.meakin@epicentre.msf.org>
2024-01-09 15:08:59 +0100

    Update plot R0 and documentation

--- R/visualisation.R
+++ R/visualisation.R
@@ -1,15 +1,14 @@
 #' Visualise change in R0
 #' @author Sophie Meakin
 #' 
-#' @description Visualise relative of risk of becoming a case versus average number of secondary cases for a % of HRCs that receive PEP (`p`).
+#' @description Visualise relative of risk of becoming a case versus average number of secondary cases.
 #' 
 #' @param sweep_summary Output from summarise_results().
-#' @param p Proportion of HRCs that receive PEP.
 #' 
 #' @return A ggplot object.
 #' @export
 #' 
-plot_r0 <- function(sweep_summary, p) {
+plot_r0 <- function(sweep_summary) {
   
   baseline_r0 <- sweep_summary %>%
     dplyr::filter(prop_pep == 0) %>%
@@ -22,9 +21,8 @@ plot_r0 <- function(sweep_summary, p) {
     )
   
   out <- sweep_summary %>%
-    dplyr::filter(prop_pep %in% c(0, p)) %>%
-    dplyr::mutate(scenario_pep = ifelse(prop_pep == 0, "Baseline", "PEP")) %>%
-    dplyr::group_by(scenario, scenario_pep, n_initialcases, prop_pep, perc_risk) %>%
+    dplyr::filter(prop_pep > 0) %>%
+    dplyr::group_by(scenario, n_initialcases, prop_pep, perc_risk) %>%
     dplyr::summarise(
       r0_mean = mean(r0),
       r0_median = median(r0),
@@ -33,24 +31,20 @@ plot_r0 <- function(sweep_summary, p) {
       r0_q75 = quantile(r0, 0.75),
       r0_q975 = quantile(r0, 0.975)
     ) %>%
-    ggplot(aes(x = perc_risk, col = scenario_pep)) +
-    geom_hline(yintercept = baseline_r0$r0_mean, lty = 3, col = "tomato") +
+    ggplot(aes(x = perc_risk, col = as.factor(prop_pep))) +
     geom_hline(yintercept = 1, lty = 2, col = "lightgrey") +
+    geom_line(aes(y = r0_mean)) +
     geom_point(aes(y = r0_mean), size = 3) +
-    geom_errorbar(aes(y = r0_mean, ymin = r0_q025, ymax = r0_q975), width = 0, lwd = 0.5) +
-    geom_errorbar(aes(y = r0_mean, ymin = r0_q25, ymax = r0_q75), width = 0, lwd = 1.5) +
-    facet_grid(~scenario_pep, scales = "free_x", space = "free_x") +
-    scale_x_continuous(breaks = seq(0, 1, 0.1)) +
+    # geom_errorbar(aes(y = r0_mean, ymin = r0_q25, ymax = r0_q75), width = 0, lwd = 1.5) +
+    scale_x_continuous(breaks = seq(0, 1, 0.2)) +
     scale_y_continuous(breaks = seq(0, 5, 0.2)) +
-    scale_color_manual(
-      values = c(
-        "Baseline" = "tomato",
-        "PEP" = "grey0"
-      )
-    ) +
-    labs(x = "Relative risk of becoming a case", y = "R0") +
+    scale_color_brewer(palette = "Reds") +
+    labs(
+      x = "Relative risk of HRCs becoming a case (PEP vs. no PEP)", y = "Average number of secondary cases (R0)",
+      col = "Probability of receiving PEP"
+      ) +
     theme_bw() +
-    theme(legend.position = "none") 
+    theme(legend.position = "top") 
   
   return(out)
   
