a26b4c5cbeebbaef05e12d924184beeac023dcb6
sophiemeakin <sophie.meakin@epicentre.msf.org>
2024-01-09 11:24:35 +0100

    Update vis (mean + 50/95% quantiles)

--- R/visualisation.R
+++ R/visualisation.R
@@ -7,6 +7,7 @@
 #' @param p Proportion of HRCs that receive PEP.
 #' 
 #' @return A ggplot object.
+#' @export
 #' 
 plot_r0 <- function(sweep_summary, p) {
   
@@ -23,10 +24,21 @@ plot_r0 <- function(sweep_summary, p) {
   out <- sweep_summary %>%
     dplyr::filter(prop_pep %in% c(0, p)) %>%
     dplyr::mutate(scenario_pep = ifelse(prop_pep == 0, "Baseline", "PEP")) %>%
-    ggplot(aes(x = perc_risk, y = r0, group = perc_risk)) +
-    geom_hline(yintercept = baseline_r0$r0_median, lty = 3, col = "tomato") +
+    dplyr::group_by(scenario, scenario_pep, n_initialcases, prop_pep, perc_risk) %>%
+    dplyr::summarise(
+      r0_mean = mean(r0),
+      r0_median = median(r0),
+      r0_q025 = quantile(r0, 0.025),
+      r0_q25 = quantile(r0, 0.25),
+      r0_q75 = quantile(r0, 0.75),
+      r0_q975 = quantile(r0, 0.975)
+    ) %>%
+    ggplot(aes(x = perc_risk, col = scenario_pep)) +
+    geom_hline(yintercept = baseline_r0$r0_mean, lty = 3, col = "tomato") +
     geom_hline(yintercept = 1, lty = 2, col = "lightgrey") +
-    geom_boxplot(aes(col = scenario_pep)) +
+    geom_point(aes(y = r0_mean), size = 3) +
+    geom_errorbar(aes(y = r0_mean, ymin = r0_q025, ymax = r0_q975), width = 0, lwd = 0.5) +
+    geom_errorbar(aes(y = r0_mean, ymin = r0_q25, ymax = r0_q75), width = 0, lwd = 1.5) +
     facet_grid(~scenario_pep, scales = "free_x", space = "free_x") +
     scale_x_continuous(breaks = seq(0, 1, 0.1)) +
     scale_y_continuous(breaks = seq(0, 5, 0.2)) +
